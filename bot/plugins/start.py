201e1ed010651cfcc9766952329d295e49f8890e
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2024-08-04 13:05:35 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,25 +1,34 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+import random
 from ..screenshotbot import ScreenShotBot
 
+PICS = [
+ "https://telegra.ph/file/9ba2b9a7785b2345b4c30.jpg",
+ "https://telegra.ph/file/313f5cdc9caae2b1a6fb3.jpg",
+ "https://telegra.ph/file/b90945a0324165879af0b.jpg"
+]
 
 @ScreenShotBot.on_message(filters.private & filters.command("start"))
 async def start(c, m):
 
-    await m.reply_text(
-        text=f"Hi there {m.from_user.mention}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from "
-        "your video files without downloading the entire file (almost instantly). For more details check /help.",
+    await m.reply_photo(
+        photo=random.choice(PICS),
+        caption=f"""Hᴇʟʟᴏ {m.from_user.mention} ✨,
+        
+I Aᴍ A Pᴏᴡᴇʀғᴜʟ Sᴄʀᴇᴇɴsʜᴏᴛ Gᴇɴᴇʀᴀᴛᴏʀ Bᴏᴛ. I Cᴀɴ Gᴇɴᴇʀᴀᴛᴇ Sᴄʀᴇᴇɴsʜᴏᴛs, Sᴀᴍᴘʟᴇs, Tʀɪᴍ Vɪᴅᴇᴏ, Aɴᴅ Sʜᴀʀᴇ Mᴇᴅɪᴀ Iɴғᴏʀᴍᴀᴛɪᴏɴ.
+
+Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ : @mrprincebotx""",       
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Source 😒", url="https://github.com/odysseusmax/animated-lamp"
+                        "Sᴏᴜʀᴄᴇ 😉", url="https://github.com/odysseusmax/animated-lamp"
                     ),
-                    InlineKeyboardButton("Project Channel", url="https://t.me/odbots"),
+                    InlineKeyboardButton("Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ✨", url="https://t.me/mrprincebotx"),
                 ],
-                [InlineKeyboardButton("My Father", url="https://t.me/odysseusmax")],
+                [InlineKeyboardButton("Dᴇᴠᴇʟᴏᴘᴇʀ 👨‍💻", url="https://t.me/YourPrince_X")],
             ]
         ),
     )
